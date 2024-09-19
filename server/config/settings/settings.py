5b9f2149e61023d9871c2a7b569ca2bb85fd7c86
YaroslavIonin <ioninyaroslav22@gmail.com>
2024-09-19 21:10:02 +0500

    Init project

---
+++ server/config/settings/settings.py
@@ -0,0 +1,134 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+
+import dj_database_url
+from dotenv import load_dotenv
+
+# load_dotenv(dotenv_path='../../../.env')  # Load environment variables
+
+BASE_DIR = Path(__file__).parents[2]
+SECRET_KEY = 'django-insecure-v%r52)ezz13#aocuwgg+1)#(md2lyob^m^)d5f*4=iq-0iktg1'
+ROOT_URLCONF = 'config.urls'
+WSGI_APPLICATION = 'config.wsgi.application'
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+SITE_ID = 1
+
+##################################################################
+# Debug settings (with docker)
+##################################################################
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+##################################################################
+# Databases settings (with docker)
+##################################################################
+
+DATABASES = {'default': dj_database_url.config()}
+
+###################################################################
+# Templates, middleware settings
+##################################################################
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.joinpath('templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = []
+
+CORS_ALLOW_CREDENTIALS = True
+
+#################################################################
+# Password validation settings
+#################################################################
+
+if not DEBUG:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+##################################################################
+# Static files settings (CSS, JavaScript, Images)
+##################################################################
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_DIRS = ('static',)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+
+FILE_UPLOAD_PERMISSIONS = 0o777
+FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o777
+
+##################################################################
+# REST FRAMEWORK
+##################################################################
+
+ANONYMOUS_USER_THROTTLE_RATE = os.environ.get('ANONYMOUS_USER_THROTTLE_RATE', 10_000)
+AUTHENTICATED_USER_THROTTLE_RATE = os.environ.get('AUTHENTICATED_USER_THROTTLE_RATE', 10_000)
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ROTATE_REFRESH_TOKENS": True,
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
+##################################################################
+# Custom user settings
+##################################################################
+
+AUTH_USER_MODEL = 'users.User'
+
+##################################################################
+# Default auto field settings
+##################################################################
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
